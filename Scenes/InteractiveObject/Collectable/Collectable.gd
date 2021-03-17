198d9498c19841b5d6139d32ff0eb969bb5a6d74
Weact <l.druckes@ludus-academie.com>
2021-03-17 22:28:25 +0100

    Fixed collectables issues

--- Scenes/InteractiveObject/Collectable/Collectable.gd
+++ Scenes/InteractiveObject/Collectable/Collectable.gd
@@ -92,6 +92,8 @@ func _on_follow_area_body_entered(body: PhysicsBody2D):
 		follow(body)
 
 func _on_collect_area_body_entered(body: PhysicsBody2D):
+	if body == null:
+		return
 	if body.is_class("Player"):
 		collect()
 
