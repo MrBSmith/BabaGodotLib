702d4a0349a09869054f07a199ccf226f3350762
Hugo Baba <Hugobarette@gmail.com>
2021-07-26 17:43:21 +0200

    Utils.trim_image optimisation

--- Statics/Utils.gd
+++ Statics/Utils.gd
@@ -23,24 +23,41 @@ static func trim_image(image: Image) -> Image:
 		var w_array = range(image.get_width()) if i == 0 else range(image.get_width() - 1, 1, -1)
 		var h_array = range(image.get_height()) if i == 0 else range(image.get_height() - 1, 1, -1)
 		
-		# REFACTO NEEDED
 		for w in w_array:
+			var found = false
 			for h in h_array:
 				var pixel = image.get_pixel(w, h)
 				if pixel.a != 0.0:
 					if i == 0:
-						if w < smallest_x && smallest_x == INF : smallest_x = w
+						if w < smallest_x && smallest_x == INF: 
+							smallest_x = w
+							found = true 
 					else:
-						if w > biggest_x && biggest_x == -1: biggest_x = w
+						if w > biggest_x && biggest_x == -1: 
+							biggest_x = w
+							found = true
+				if found:
+					break
+			if found:
+				break
 		
 		for h in h_array:
+			var found = false
 			for w in w_array:
 				var pixel = image.get_pixel(w, h)
 				if pixel.a != 0.0:
 					if i == 0:
-						if h < smallest_y && smallest_y == INF : smallest_y = h
+						if h < smallest_y && smallest_y == INF : 
+							smallest_y = h
+							found = true
 					else:
-						if h > biggest_y && biggest_y == -1: biggest_y = h
+						if h > biggest_y && biggest_y == -1: 
+							biggest_y = h
+							found = true
+				if found:
+					break
+			if found:
+				break
 	
 	
 	var output_img = Image.new()
