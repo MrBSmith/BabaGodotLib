46722824e49d8a940739fc1ff7bea82a9a7bb9a4
Hugo Baba <Hugobarette@gmail.com>
2021-11-25 11:38:31 +0100

    Collectable_FollowState

--- Behaviours/Collectable/FollowCollectable.gd
+++ Behaviours/Collectable/FollowCollectable.gd
@@ -23,16 +23,6 @@ func _ready() -> void:
 	var __ = $FollowArea.connect("body_entered", self, "_on_follow_area_body_entered")
 
 
-func _physics_process(delta: float) -> void:
-	match(get_state_name()):
-		"Idle":
-			pass
-		"Follow":
-			follow(delta)
-		"Collect":
-			pass
-
-
 
 #### VIRTUALS ####
 
@@ -49,20 +39,6 @@ func follow_target(new_target: Node):
 	set_target(new_target)
 	set_state("Follow")
 
-
-func follow(delta: float) -> void:
-	if target != null:
-		speed += acceleration
-		
-		var obj_pos = owner.get_position()
-		var target_pos = target.get_position()
-		var target_dir = obj_pos.direction_to(target_pos)
-		var velocity = target_dir * speed * delta
-		
-		if obj_pos.distance_to(target_pos) < speed * delta:
-			owner.set_position(target_pos)
-		else:
-			owner.set_position(obj_pos + velocity)
 
 
 #### INPUTS ####
