d9cc0d5496cd8f4eaad8c999b093ef9b7a22042e
Hugo Baba <Hugobarette@gmail.com>
2022-02-16 14:41:53 +0100

    Remove useless Factories.tscn scene & create a Cooldown class

---
+++ Time/Cooldown.gd
@@ -0,0 +1,32 @@
+tool
+extends Timer
+class_name Cooldown
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "Cooldown" or .is_class(value)
+func get_class() -> String: return "Cooldown"
+
+
+#### BUILT-IN ####
+
+func _ready() -> void:
+	if Engine.editor_hint:
+		autostart = false
+		one_shot = true
+
+
+#### VIRTUALS ####
+
+
+#### LOGIC ####
+
+func is_running() -> bool:
+	return !is_paused() && !is_stopped()
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####
