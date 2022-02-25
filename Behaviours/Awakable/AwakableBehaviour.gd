e03f65c85976f86f188f45918318b179f351264f
Hugo Baba <Hugobarette@gmail.com>
2022-02-25 15:28:35 +0100

    Fix AwakableBehaviour awake & can_sleep interaction

--- Behaviours/Awakable/AwakableBehaviour.gd
+++ Behaviours/Awakable/AwakableBehaviour.gd
@@ -17,10 +17,7 @@ func get_class() -> String: return "AwakableBehaviour"
 
 #### BUILT-IN ####
 
-func _ready() -> void:
-	yield(owner, "ready")
-	
-	var __ = owner.connect("sleeping_state_changed", self, "_on_owner_sleeping_state_changed")
+
 
 #### VIRTUALS ####
 
@@ -33,10 +30,11 @@ func awake() -> void:
 	if disabled:
 		return
 	
-	if not owner is PhysicsBody2D && owner.get_mode() != RigidBody2D.MODE_STATIC:
+	if not owner is PhysicsBody2D:
 		return
-
+	
 	owner.set_mode(RigidBody2D.MODE_RIGID)
+	
 	owner.set_sleeping(false)
 	owner.set_physics_process(true)
 	
@@ -47,13 +45,13 @@ func asleep() -> void:
 	if disabled:
 		return
 
-	if not owner is PhysicsBody2D && owner.get_mode() != RigidBody2D.MODE_RIGID:
+	if not owner is PhysicsBody2D && owner.get_mode() == RigidBody2D.MODE_STATIC:
 		return
-	
+
 	if !owner.can_sleep:
 		return
-	
-	owner.set_mode(RigidBody2D.MODE_STATIC)
+
+	owner.set_mode(RigidBody2D.MODE_RIGID)
 	owner.set_sleeping(true)
 	owner.set_physics_process(false)
 	
@@ -67,7 +65,3 @@ func asleep() -> void:
 #### SIGNAL RESPONSES ####
 
 
-# Set the mode back to static mode when the body is sleeping
-func _on_owner_sleeping_state_changed():
-	if owner.get_mode() == RigidBody2D.MODE_RIGID && owner.is_sleeping():
-		owner.call_deferred("set_mode", RigidBody2D.MODE_STATIC)
