8f6f501c0ce46134afd00fb43918631d5c84dcaa
Hugo Baba <Hugobarette@gmail.com>
2022-09-30 09:10:37 +0200

    Add a color for disabled points in AstarVisualizer

--- Debug/Astar2DVisualizer.gd
+++ Debug/Astar2DVisualizer.gd
@@ -4,10 +4,10 @@ class_name Astar2DVisualizer
 var astar : AStar2D
 export var cell_size := Vector2.ONE
 
-export var connections_color := Color.blue
-export var connection_width : float = 3.0
+export var normal_color := Color.blue
+export var disabled_coloe := Color.red
 
-export var points_color := Color.blue
+export var connection_width : float = 3.0
 export var points_radius : float = 4.0
 
 export var id_key : int = 666
@@ -34,12 +34,13 @@ func _draw() -> void:
 	
 	for point_id in astar.get_points():
 		var point = astar.get_point_position(point_id)
-		draw_circle(point * cell_size, points_radius, points_color)
+		var color = normal_color if !astar.is_point_disabled(point_id) else disabled_coloe
+		draw_circle(point * cell_size, points_radius, color)
 		
 		for connected_point_id in astar.get_point_connections(point_id):
 			var connected_point = astar.get_point_position(connected_point_id)
 			
-			draw_line(point * cell_size, connected_point * cell_size, connections_color, connection_width)
+			draw_line(point * cell_size, connected_point * cell_size, normal_color, connection_width)
 
 
 
