a469309df71f93bfabb59fad2b88a4213926da54
Hugo Baba <Hugobarette@gmail.com>
2022-11-23 15:15:17 +0100

    Add Utils;are_event_same_input() method

--- Statics/Utils.gd
+++ Statics/Utils.gd
@@ -296,3 +296,23 @@ static func get_input_event_as_text(event: InputEvent) -> String:
 		return OS.get_scancode_string(OS.keyboard_get_scancode_from_physical(event.physical_scancode))
 	else:
 		return event.as_text()
+
+
+static func are_event_same_input(event_a: InputEvent, event_b: InputEvent) -> bool:
+	if event_a.get_class() != event_b.get_class():
+		return false
+	
+	if event_a is InputEventKey:
+		return (event_a.scancode == event_b.scancode && event_a.scancode != 0) \
+		or (event_a.physical_scancode == event_b.physical_scancode && event_a.physical_scancode != 0)
+	
+	elif event_a is InputEventJoypadButton:
+		return event_a.button_index == event_b.button_index && event_a.device == event_b.device
+	
+	elif event_a is InputEventJoypadMotion:
+		return event_a.axis == event_b.axis && sign(event_a.axis_value) == sign(event_b.axis_value) \
+			 && event_a.device == event_b.device
+	
+	return false
+
+
