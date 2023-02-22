ce657d15d241a7b7d57da003a1943e5143ca4240
Hugo Baba <Hugobarette@gmail.com>
2023-02-22 15:36:05 +0100

    feat(LevelEvent): Add support for area triggered events

--- Event/LevelEvent.gd
+++ Event/LevelEvent.gd
@@ -1,8 +1,7 @@
 extends Event
 class_name LevelEvent
 
-export var target_name : String = ""
-export var target_as_group : bool = false
+export var target_path_array : Array
 export var method_name : String = ""
 export var arguments_array : Array = []
 export var queue_free_after_trigger : bool = true
@@ -12,6 +11,13 @@ export var once_per_level : bool = false
 
 func is_class(value: String) -> bool: return value == "LevelEvent" or .is_class(value)
 func get_class() -> String: return "LevelEvent"
+
+
+func _ready() -> void:
+	for child in get_children():
+		if child is Area2D:
+			child.connect("body_entered", self, "_on_area_body_entered")
+
 
 
 func event():
@@ -26,37 +32,19 @@ func event():
 # Call the given method in a single target or a group target
 # Pass any number of arguments stored in the arguments_array to this method
 func method_call():
-	# Check for an empty field, send an error if there is one
-	if target_name == "":
-		push_error("The event %s has no target_name to call" % name)
-		return
-	
-	elif method_name == "":
+	if method_name == "":
 		push_error("The event %s has no method to call" % name)
 		return
 	
-	# Get the target(s) and store it in target_array
 	var target_array : Array = []
-	if target_as_group:
-		target_array = get_tree().get_nodes_in_group(target_name)
-	else:
-		var level = GAME.current_level
-		if level == null:
-			push_error("The current level is null, the event couldn't trigger")
-			return
+	
+	for target_path in target_path_array:
+		var target = get_node_or_null(target_path)
 		
-		if level.name == target_name:
-			target_array.append(level)
-		else:
-			var target = GAME.current_level.find_node(target_name)
-			if target == null:
-				target = Utils.find_autoload(target_name, get_tree())
-			
-			if target != null:
-				target_array.append(target)
-			else:
-				push_error("No target with the name %s were found" % target_name)
+		assert(target != null, "Target cannot be found at path %s" % str(target_path))
 		
+		target_array.append(target)
+	
 	# Call the method in every target, and pass every argument in the array
 	for target in target_array:
 		if target.has_method(method_name) or GDScript:
@@ -69,3 +57,7 @@ func method_call():
 	# Queue free this event if it should be
 	if queue_free_after_trigger:
 		queue_free()
+
+
+func _on_area_body_entered(body: Node2D) -> void:
+	event()
