b47bed8fd9c5d30f9f5eee59c3b7a9ad501509ac
Hugo Baba <hugobarette@gmail.com>
2024-12-17 10:31:08 +0100

    IconMaskTransition polish

--- Transitions/IconMaskTransition.gd
+++ Transitions/IconMaskTransition.gd
@@ -2,15 +2,16 @@ extends TransitionLayer
 class_name IconMaskTransition
 
 const POLY_GEN_EPSILON = 1.5
+const NB_LAYERS = 3
+const LAYER_INTERVAL = 0.3
 
 export var dezoomed_scale := Vector2(10.0, 10.0)
 export var zoomed_scale := Vector2(0.5, 0.5)
 export var level_type_textures_dict : Dictionary = {}
 
 onready var pivot: Node2D = $"%Pivot"
-onready var polygon_container: Node2D = $Polygons
 
-var mask_poly : PoolVector2Array
+var mask_poly_array : Array
 
 func _ready() -> void:
 	var __ = EVENTS.connect("level_transition", self, "_trigger")
@@ -30,11 +31,16 @@ func _trigger(level_type: String) -> void:
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(image)
 	var mask_rect = Rect2(Vector2.ONE, bitmap.get_size())
-	mask_poly = bitmap.opaque_to_polygons(mask_rect, POLY_GEN_EPSILON)[0]
+	var mask_poly = bitmap.opaque_to_polygons(mask_rect, POLY_GEN_EPSILON)[0]
 	var mask_offset = (GAME.window_size - image.get_size()) / 2.0
 	
 	for i in range(mask_poly.size()):
 		mask_poly[i] += mask_offset
+	
+	mask_poly_array = []
+	
+	for i in range(NB_LAYERS):
+		mask_poly_array.append(mask_poly)
 	
 	fade()
 	
@@ -44,7 +50,7 @@ func _trigger(level_type: String) -> void:
 	hide()
 
 
-func fade(fade_time := 2.3, fade_mode : int = FADE_MODE.FADE_IN_OUT, delay := 0.0, pause_time := 1.0) -> void:
+func fade(fade_time := 2.0, fade_mode : int = FADE_MODE.FADE_IN_OUT, delay := 0.0, pause_time := 0.5) -> void:
 	if tween:
 		tween.kill()
 	
@@ -54,15 +60,16 @@ func fade(fade_time := 2.3, fade_mode : int = FADE_MODE.FADE_IN_OUT, delay := 0.
 	var duration = fade_time / 2.0 if fade_mode == FADE_MODE.FADE_IN_OUT else fade_time
 	
 	if fade_mode != FADE_MODE.FADE_IN:
-		var __ = tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-		__ = tween.tween_method(self, "_set_mask_scale", dezoomed_scale, zoomed_scale, duration).set_delay(delay)
+		for i in range(NB_LAYERS):
+			var __ = tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+			__ = tween.parallel().tween_method(self, "_set_mask_scale", dezoomed_scale, zoomed_scale, duration, [i]).set_delay(LAYER_INTERVAL * i + delay)
 		
 		yield(tween, "finished")
 		
 		if fade_mode == FADE_MODE.FADE_IN_OUT:
 			EVENTS.emit_signal("transition_middle")
 	
-	if fade_mode == FADE_MODE.FADE_IN_OUT && pause_time > 0.0:
+	if fade_mode == FADE_MODE.FADE_IN_OUT and pause_time > 0.0:
 		yield(get_tree().create_timer(pause_time), "timeout")
 		EVENTS.emit_signal("transition_pause_finished")
 	
@@ -74,22 +81,26 @@ func fade(fade_time := 2.3, fade_mode : int = FADE_MODE.FADE_IN_OUT, delay := 0.
 			tween.kill()
 		
 		tween = create_tween()
-		var __ = tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
-		__ = tween.tween_method(self, "_set_mask_scale", zoomed_scale, dezoomed_scale, duration).set_delay(delay)
+		for i in range(NB_LAYERS):
+			var id = NB_LAYERS - i - 1
+			var __ = tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+			__ = tween.parallel().tween_method(self, "_set_mask_scale", zoomed_scale, dezoomed_scale, duration, [id]).set_delay(LAYER_INTERVAL * i + delay)
 		
-		yield(get_tree().create_timer(delay + duration * 0.5), "timeout")
+		yield(tween, "finished")
 	
 	running = false
 	EVENTS.emit_signal("transition_finished")
 
 
-func _set_mask_scale(mask_scale: Vector2) -> void:
+func _set_mask_scale(mask_scale: Vector2, mask_id: int) -> void:
 	var poly = PoolVector2Array()
-	poly.resize(mask_poly.size())
+	poly.resize(mask_poly_array[mask_id].size())
 	var center = GAME.window_size / 2.0
 	
-	for i in range(mask_poly.size()):
-		poly[i] = (mask_poly[i] - center) * mask_scale + center
+	for i in range(mask_poly_array[mask_id].size()):
+		poly[i] = (mask_poly_array[mask_id][i] - center) * mask_scale + center
+	
+	var polygon_container: Node2D = $Layers.get_child(mask_id)
 	
 	for half in polygon_container.get_children():
 		var screen_poly = half.get_node("Screen")
@@ -103,11 +114,3 @@ func _set_mask_scale(mask_scale: Vector2) -> void:
 				result.set_polygon(result_polygons[id])
 			else:
 				result.set_polygon(PoolVector2Array())
-#
-#		if result_polygons.empty():
-#			if mask_scale < Vector2.ONE:
-#				result = screen_poly.polygon
-#		else:
-#			result = result_polygons[0]
-		
-#		result_poly.set_polygon(result)
