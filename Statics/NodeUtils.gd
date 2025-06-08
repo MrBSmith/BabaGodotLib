296a90541dfaa8a09b71f6b1b0b3ac6c6096ce38
Hugo Baba <hugobarette@gmail.com>
2025-06-08 16:37:01 +0200

    Make NodeFinder & TraitFinder better

---
+++ Statics/NodeUtils.gd
@@ -0,0 +1,39 @@
+extends Node
+class_name NodeUtils
+
+static func clear(node: Node) -> void:
+	var last_child = null
+
+	for child in node.get_children():
+		last_child = child
+		child.queue_free.call_deferred()
+
+	if last_child and last_child.is_inside_tree():
+		await last_child.tree_exited
+
+
+static func find(node: Node, wanted_class: Variant) -> Array:
+	var array = []
+	for child in node.get_children():
+		if is_instance_of(node, wanted_class):
+			array.append(child)
+	return array
+
+
+static func find_first(node: Node, wanted_class: Variant) -> Node:
+	for child in node.get_children():
+		if is_instance_of(node, wanted_class):
+			return child
+	return null
+
+
+static func find_recursive(node: Node, wanted_class: Variant, array: Array = []) -> Array:
+	for child in node.get_children():
+		if is_instance_of(node, wanted_class) and not child in array:
+			array.append(child)
+		
+		if child.get_child_count() > 0:
+			var __ = find_recursive(child, wanted_class, array)
+	
+	return array
+
--- Statics/TraitFinder.gd
+++ Statics/TraitFinder.gd
@@ -1,12 +1,10 @@
 extends Node
 class_name TraitFinder
 
-static func find_trait(node: Node, trait_name: String) -> Trait:
-	if node.has_meta(trait_name):
-		return node.get_meta(trait_name, null) as Trait
-	else:
-		return null
+static func find_trait(node: Node, trait_class: Variant) -> Trait:
+	var id = node.get_children().find_custom(func(n: Node): return is_instance_of(n, trait_class))
+	return node.get_child(id) if id != -1 else null
 
 
-static func has_trait(node: Node, trait_name: String) -> bool:
-	return node.has_meta(trait_name)
+static func has_trait(node: Node, trait_class: Variant) -> bool:
+	return find_trait(node, trait_class) != null
