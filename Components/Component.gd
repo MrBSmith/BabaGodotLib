fc8649ad559e963295989dbe2451fb33048d8e8a
Hugo Baba <hugobarette@gmail.com>
2025-08-07 14:21:02 +0200

    factor(Misc): 🛠️ Rename trait_name -> component_name

--- Components/Component.gd
+++ Components/Component.gd
@@ -4,12 +4,11 @@ class_name Component
 @export var holder_path := NodePath()
 @onready var holder = owner if holder_path.is_empty() else get_node_or_null(holder_path)
 
-
 func _ready() -> void:
-	var trait_name = get_trait_name()
-	holder.add_to_group(trait_name)
-	holder.set_meta(trait_name, self)
+	var component_name = get_component_name()
+	holder.add_to_group(component_name)
+	holder.set_meta(component_name, self)
 
 
-func get_trait_name() -> String:
+func get_component_name() -> String:
 	return get_script().get_global_name()
